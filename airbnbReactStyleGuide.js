845bfcabd79d1457dc923b029996a3794e8d1ea6
mrhdman <mattrhavins@gmail.com>
2019-05-02 09:44:31 -0400

    Class vs React.createClass vs stateless

--- airbnbReactStyleGuide.js
+++ airbnbReactStyleGuide.js
@@ -11,3 +11,45 @@
 //  - Do not use React.createElement unless you're initializing the app from a file that is not JSX
 //  - react/forbid-prop-types will allow arrays and objects only if it is explicitly noted what array and object contains, using arrayOf, objectOf, or shape.
 
+
+// 2. Class vs React.createClass vs stateless
+// 
+// - If you have internal state and/or refs, prefer class extends React.Component over React.createClass . eslint: react/prefer-es6-class react/prefer-stateless-function
+
+
+// ex)
+// bad
+const Listing = React.createClass({
+    // ... 
+    render() {
+        return <div>{this.state.hello}</div>;
+    }
+});
+
+// good
+class Listing extends React.Component {
+    // ... 
+    render() {
+        return <div>{this.state.hello}</div>
+    }
+}
+
+// - And if you don't have state or refs, prefer normal functions (not arrow functions) over class:
+
+// bad
+class Listing extends React.Component {
+    render() {
+        return <div>{this.props.hello}</div>
+    }
+}
+
+// bad (relying on function name inference is discouraged)
+const Listing = ({ hello }) => {
+    <div>{hello}</div>
+};
+
+// good
+function Listing({ hello }) {
+    return <div>{hello}</div>;
+}
+
