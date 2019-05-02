13e06b84492922ad13a71a5ea04ff66c59870cdf
mrhdman <mattrhavins@gmail.com>
2019-05-02 09:34:59 -0400

    Basic Rules

---
+++ airbnbReactStyleGuide.js
@@ -0,0 +1,13 @@
+// Airbnb React/JSX Style Guide
+
+
+
+
+
+// 1. Basic Rules
+//  - Only include one React component per file.
+//      - However, multiple Stateless, or Pure, Components are allowed per file. eslint: react/no-multi-comp
+//  - Always use JSX syntax.
+//  - Do not use React.createElement unless you're initializing the app from a file that is not JSX
+//  - react/forbid-prop-types will allow arrays and objects only if it is explicitly noted what array and object contains, using arrayOf, objectOf, or shape.
+
